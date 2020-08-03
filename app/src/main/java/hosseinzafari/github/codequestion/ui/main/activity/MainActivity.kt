9c572dbf6e4c47d887170a12811035f56e09572d
Hossein <ho3einzafari@gmail.com>
2020-08-03 13:57:26 +0430

    Add UiUtil for easy work on fragment

--- app/src/main/java/hosseinzafari/github/codequestion/ui/main/activity/MainActivity.kt
+++ app/src/main/java/hosseinzafari/github/codequestion/ui/main/activity/MainActivity.kt
@@ -1,43 +1,42 @@
 package hosseinzafari.github.codequestion.ui.ui.main.activity
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import com.luseen.spacenavigation.SpaceItem
 import com.luseen.spacenavigation.SpaceOnClickListener
 import hosseinzafari.github.codequestion.R
-import hosseinzafari.github.codequestion.ui.ui.main.fragment.FactoryFragment
 import hosseinzafari.github.codequestion.ui.helper.log
+import hosseinzafari.github.codequestion.ui.ui.main.fragment.FactoryFragment
+import hosseinzafari.github.codequestion.ui.util.UiUtil
 import hosseinzafari.github.framework.core.ui.activity.GAppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : GAppCompatActivity() {
 
+    private lateinit var uiUtil: UiUtil
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fm = supportFragmentManager
-        val transaction = fm.beginTransaction()
-        transaction.add(R.id.framelayout , getFragment(FactoryFragment.HOME_FRAGMENT)).addToBackStack("Home").commit()
+        // init first fragment
+        uiUtil = UiUtil(this)
+        uiUtil.addFragment(FactoryFragment.HOME_FRAGMENT)
 
         // Setup Navigation View
         navigation_view.initWithSaveInstanceState(savedInstanceState)
         initNavigationView()
     }
 
-    private fun getFragment(byte: Byte): Fragment {
-        return FactoryFragment.getFragment(byte)
-    }
 
     // Prepare And Customize Navigation View
     private fun initNavigationView(){
-        navigation_view.apply { 
+        navigation_view.apply {
             addSpaceItem(SpaceItem("پروفایل" , R.drawable.ic_people_white_24dp))
             addSpaceItem(SpaceItem("کد ها" , R.drawable.ic_code_white_24dp))
             addSpaceItem(SpaceItem("سوالات" , R.drawable.ic_question_white_24dp))
             addSpaceItem(SpaceItem("خانه" , R.drawable.ic_home_white_24dp))
             showIconOnly()
+            changeCurrentItem(3)
             setSpaceOnClickListener(object  : SpaceOnClickListener{
                 override fun onCentreButtonClick() {
                     log("onCentreButton Clicked!")
@@ -49,6 +48,10 @@ class MainActivity : GAppCompatActivity() {
     
                 override fun onItemClick(itemIndex: Int, itemName: String?) {
                     log("onItemClick itemIndex $itemIndex , itemName $itemName")
+                    when(itemIndex){
+                        3-> uiUtil.replaceFragment(FactoryFragment.HOME_FRAGMENT)
+                        2-> uiUtil.replaceFragment(FactoryFragment.LOGIN_FRAGMENT)
+                    }
                 }
             })
         }
---
+++ app/src/main/java/hosseinzafari/github/codequestion/ui/util/UiUtil.kt
@@ -0,0 +1,33 @@
+package hosseinzafari.github.codequestion.ui.util
+
+import androidx.fragment.app.FragmentActivity
+import hosseinzafari.github.codequestion.R
+import hosseinzafari.github.codequestion.ui.ui.main.fragment.FactoryFragment
+
+/*
+
+@created in 03/08/2020 - 10:33 AM
+@project Code Question
+@author Hossein Zafari 
+@email  hosseinzafari2000@gmail.com 
+*/
+
+class UiUtil(val activity: FragmentActivity) {
+
+
+    fun getFragment(byte: Byte) = FactoryFragment.getFragment(byte)
+
+    fun addFragment(targetFragment: Byte , containerLayout: Int = R.id.framelayout , tag: String? = null) {
+        activity.supportFragmentManager.beginTransaction().add(containerLayout , getFragment(targetFragment) , tag).commit()
+    }
+
+    fun replaceFragment(targetFragment: Byte, containerLayout: Int = R.id.framelayout, tag: String? = null) {
+        activity.supportFragmentManager.beginTransaction().replace(containerLayout , getFragment(targetFragment) , tag).commit()
+    }
+
+    fun replaceFragmentWithBack(targetFragment: Byte, containerLayout: Int = R.id.framelayout, tag: String? = null) {
+        activity.supportFragmentManager.beginTransaction().addToBackStack(tag).replace(containerLayout , getFragment(targetFragment) , tag)?.commit()
+    }
+
+
+}

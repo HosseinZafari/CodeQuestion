704dacaa1dec360d08e0b2cec9cc4e23fb540ce1
Hossein Zafari <ho3einzafari@gmail.com>
2020-09-09 23:36:56 +0430

    Add DataModel ProfileViewModel

---
+++ app/src/main/java/hosseinzafari/github/codequestion/struct/ProfileViewPagerModel.kt
@@ -0,0 +1,14 @@
+package hosseinzafari.github.codequestion.struct
+
+/*
+
+@created in 02/09/2020 - 07:21 PM
+@project Code Question
+@author Hossein Zafari 
+@email  hosseinzafari2000@gmail.com 
+*/
+
+data class ProfileViewPagerModel(
+    val fragmentId: Byte,
+    val title: String
+)
--- app/src/main/java/hosseinzafari/github/codequestion/struct/UserModel.kt
+++ app/src/main/java/hosseinzafari/github/codequestion/struct/UserModel.kt
@@ -2,14 +2,14 @@ package hosseinzafari.github.codequestion.ui.struct
 
 
 data class UserModel(
-    val userId: String,  // 2
+    val userId: String? = null,  // 2
     val name: String? = null,   // حسین
     val family: String? = null, // ظفری
-    val phone: String? = null,
+    val phoneNumber: String? = null,
     val email: String? = null,
     val role: String? = null,
     val gender: Byte? = 0,  // 0 or 1 == male or female
     val image: String? = null,  // https://ware.uncox.com/asset/profile/male/15.jpg
     val point: String? = null ,  // 40
-    val token: String
+    val token: String? = null
 )

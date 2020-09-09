3ab4a75f06f7b5c269388409aea74a3e88c3d1be
Hossein Zafari <ho3einzafari@gmail.com>
2020-09-09 23:20:20 +0430

    Add AccountFragment In ProfileFragment [ViewPager]

---
+++ app/src/main/java/hosseinzafari/github/codequestion/ui/main/fragment/AccountFragment.kt
@@ -0,0 +1,44 @@
+package hosseinzafari.github.codequestion.ui.main.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import hosseinzafari.github.codequestion.R
+import hosseinzafari.github.framework.core.ui.fragment.GFragment
+
+/*
+
+@created in 03/09/2020 - 10:33 AM
+@project Code Question
+@author Hossein Zafari 
+@email  hosseinzafari2000@gmail.com 
+*/
+
+class AccountFragment : GFragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_account , container , false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupViews(view)
+    }
+
+    private fun setupViews(view: View) {
+//        val tabLayout = view.findViewById<TabLayout>(R.id.tablayout_profile)
+//        val txt_name = view.findViewById<TextView>(R.id.txt_profile_name)
+//        val tabLayout = view.findViewById<TabLayout>(R.id.tablayout_profile)
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+    }
+}
